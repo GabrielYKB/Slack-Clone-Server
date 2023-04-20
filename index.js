8762312f2f5951b6bae60df488473753b0518a15
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-04-20 16:02:20 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,86 @@
+import express from "express";
+const app = express();
+
+import mongoose from "mongoose";
+import cors from "cors";
+
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Gabriel");
+});
+
+const channelsShecma = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 30,
+    },
+  },
+  { timestamps: true }
+);
+
+const Channel = mongoose.model("channels", channelsShecma);
+
+app.get("/channels", async (req, res) => {
+  const channels = await Channel.find();
+  res.send(channels);
+});
+
+const messageSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+      minLength: 1,
+    },
+    user: {
+      name: {
+        type: String,
+        required: true,
+      },
+      image: {
+        type: String,
+        required: true,
+      },
+    },
+    channelId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "channels",
+    },
+  },
+  { timestamps: true }
+);
+const Message = mongoose.model("messages", messageSchema);
+
+app.get("/channels/:id", async (req, res) => {
+  const channel = await Channel.findById(req.params.id);
+  const messages = await Message.find({ channelId: req.params.id }).sort({
+    createdAt: "desc",
+  });
+
+  res.send({ channel, messages });
+});
+
+app.post("/channels/:id", async (req, res) => {
+  console.log(req.body);
+  await Message.create({
+    text: req.body.text,
+    user: {
+      name: req.body.username,
+      image:
+        "https://api.dicebear.com/5.x/adventurer/svg?seed=" + req.body.username,
+    },
+    channelId: req.params.id,
+  });
+  res.send("oki");
+});
+
+app.listen(3000, async () => {
+  await mongoose.connect(
+    "mongodb+srv://gab:gab@cluster0.r3xwl1z.mongodb.net/?retryWrites=true&w=majority"
+  );
+});
